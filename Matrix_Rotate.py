994c7125ed3c424cc536683f9e3d2583a950b7f1
abansal782 <39608806+abansal782@users.noreply.github.com>
2018-08-19 12:13:55 +0530

    Update Matrix_Rotate.py

--- Matrix_Rotate.py
+++ Matrix_Rotate.py
@@ -2,21 +2,21 @@ def rotate(img,rows,columns,flag):
     img3=[]
     if flag==1:
 #Moving 90 degree clock-wise
-        for i in range(rows):
+        for i in range(columns):
             d=rows-1
             img2=[]
-            for j in range(columns):
+            for j in range(rows):
                 img2.append(img[d][i])
                 d=d-1
             img3.append(list(img2))
         return img3
     elif flag==0:
 #Moving 90 degree anticlockwise direction
-        for i in range(rows):
-            d=rows-1-i
+        for i in range(columns):
+            d=columns-1-i
 #            print(d)
             img2=[]
-            for j in range(columns):
+            for j in range(rows):
                 img2.append(img[j][d])
             img3.append(list(img2))
         return img3
@@ -25,5 +25,5 @@ def rotate(img,rows,columns,flag):
 
 
 lis=[[2,1,3],[8,4,5],[6,9,7]]
-new_lis=rotate(lis,3,3,0)
+new_lis=rotate(lis,len(lis),len(lis[0]),0)
 print("After Rotation Matrix Will be = ",new_lis)
